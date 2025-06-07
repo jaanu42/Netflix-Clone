1014f83252103d58fdb4689f394fa12de8ba1f90
Jahnavi <jaanujahnavi42@example.com>
2025-06-07 12:50:38 +0530

    Initial commit: Netflix Clone

---
+++ script.js
@@ -0,0 +1,25 @@
+//Navbar css property in scrool
+window.addEventListener('scroll',()=>{
+    const navbar=document.querySelector(".navbar");
+    if(window.scrollY>100){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+});
+//profile section redirecting
+const profileIcon=document.querySelector(".profile-icon");
+profileIcon.addEventListener("click",()=>{
+    window.location.href="profile-details.html";
+});
+//movie card
+const movieCard=document.querySelectorAll(".content-card");
+movieCard.forEach((card)=>{
+    card.addEventListener("mouseenter",()=>{
+        card.computedStyleMap.transform="scale(1.05)";
+    });
+    card.addEventListener("mouseleave",()=>{
+        card.computedStyleMap.transform="scale(1)";
+    });
+})
